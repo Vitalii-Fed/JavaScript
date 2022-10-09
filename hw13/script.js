2704dea75ed859b46d9392059b57cb40a10f5267
Vitalii-Fed <aevetal@gmail.com>
2022-10-09 19:26:25 +0300

    end hw13

--- hw13/script.js
+++ hw13/script.js
@@ -1,53 +1,47 @@
 'use strict';
 
 //! BURGERS
-const SMALL_SIZE = {
-    price: 100,
-    callories: 200,
-}
+const SMALL_SIZE = { price: 50, callories: 20 };
+const MEDIUM_SIZE = { price: 75, callories: 30 };
+const LARGE_SIZE = { price: 100, callories: 40 };
 
-const BIG_SIZE = {
-    price: 200,
-    callories: 400,
-}
+const CHEESE = { price: 10, callories: 20 };
+const POTATO = { price: 15, callories: 10 };
+const SALAD = { price: 20, callories: 5 };
+const MAYO = { price: 20, callories: 5 };
+const SPICES = { price: 15, callories: 0 };
 
-const TOPPING_MAYO = {
-    price: 15,
-    callories: 70,
-}
+const doubleBurger = new Hamburger(SMALL_SIZE);
 
-const TOPPING_KETCHUP = {
-    price: 10,
-    callories: 50,
-}
-
-const TOPPING_CHEESE = {
-    price: 20,
-    callories: 90,
-}
 
 function Hamburger({ price, callories }) {
     this.price = price;
     this.callories = callories;
 }
 
-Hamburger.prototype.addTopping = function ({ price, callories }) {
-    this.price + price;
-    this.callores + callories;
+Hamburger.prototype.addTopping = function ({ price, callories}) {
+    return {
+        price: this.price += price,
+        callories: this.callories += callories,
+    }
+}
+Hamburger.prototype.getPrice = function () {
+    return this.price;
+}
+Hamburger.prototype.getCallories = function () {
+    return this.callories;
 }
 
-Hamburger.prototype.getCallories = function ({ callories }) {
-    this.callories += this.callories;
-}
+doubleBurger.addTopping(CHEESE);
+doubleBurger.addTopping(POTATO);
+doubleBurger.addTopping(SALAD);
+doubleBurger.addTopping(MAYO);
+doubleBurger.addTopping(SPICES);
 
-Hamburger.prototype.getPrice = function ({ price }) {
-    this.price += this.price;
-}
-
-const brg = new Hamburger(BIG_SIZE);
-
-
-
+console.log(`
+Prices with additives: ${doubleBurger.getPrice()}
+Callories with additives: ${doubleBurger.getCallories()}
+`);
 
 
 
@@ -61,6 +55,23 @@ const brg = new Hamburger(BIG_SIZE);
 
 
 
+
+
+
+
+
+
+
+
+
+
+// * const hamburger = new Hamburger(SIZE_SMALL);
+//* // добавка из майонеза
+// * hamburger.addTopping(TOPPING_MAYO);
+// * hamburger.addTopping(TOPPING_POTATO);
+
+// * console.log("Price with sauce: “ + hamburger.getPrice());
+// * console.log("Callories with sauce: “ + hamburger.getCallories());
 
 
 
