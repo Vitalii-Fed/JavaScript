88d0b975ce551b6304ac554d70c5b0b67bd2e24f
Vitalii-Fed <aevetal@gmail.com>
2022-11-20 11:02:07 +0200

    fix

--- hw19/script.js
+++ hw19/script.js
@@ -1,9 +1,13 @@
 const API_URL = 'https://jsonplaceholder.typicode.com/photos?_limit=10';
-const galleryEl = $('#galleryTemplate');
+const $galleryEl = $('#galleryEl');
 const photosTemplate = `<img data-img-id="{{id}}" src="{{url}}" alt="img">`
 let photoList = [];
 
-fetchPhotoList();
+init();
+
+function init() {
+    fetchPhotoList();
+};
 
 
 function fetchPhotoList() {
@@ -15,19 +19,10 @@ function fetchPhotoList() {
     })
 }
 
-function fetchPhotoList() {
-        fetch(API_URL)
-        .then((resp) => resp.json())
-        .then((data) => {
-            photoList = data;
-            renderList(photoList);
-        })
-}
-
 function renderList(list) {
     const photos = list.map(generatePhotoHtml).join('');
 
-    return galleryEl.append(photos);
+    return $galleryEl.append(photos);
 }
 
 function generatePhotoHtml(item) {
